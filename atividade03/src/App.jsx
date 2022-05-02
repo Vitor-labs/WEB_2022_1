14b246503431a791328020e1bd61b6a1e2975d2f
Vitor Hugo <v02hx10@gmail.com>
2022-05-02 00:17:39 -0300

    MOD: Refac routes

--- atividade03/src/App.jsx
+++ atividade03/src/App.jsx
@@ -2,8 +2,8 @@ import * as React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-import Home from "./components/home";
-import About from "./components/about";
+import Home from "./components/Home";
+import About from "./components/About";
 
 import CreateStudent from "./components/student/CreateStudent";
 import ReadStudent from "./components/student/ReadStudent";
@@ -35,7 +35,7 @@ function App() {
                   <Link to="/createStudent" className="nav-link">Criar Estudante</Link>
                 </li>
                 <li className="navitem">
-                  <Link to="/readStudent" className="nav-link">Listar Estudante</Link>
+                  <Link to="/listStudent" className="nav-link">Listar Estudante</Link>
                 </li>
               </ul>
             </li>
@@ -48,7 +48,7 @@ function App() {
                   <Link to="/createProfessor" className="nav-link">Criar Professor</Link>
                 </li>
                 <li className="navitem">
-                  <Link to="/readProfessor" className="nav-link">Listar Professor</Link>
+                  <Link to="/listProfessor" className="nav-link">Listar Professor</Link>
                 </li>
               </ul>
             </li>
@@ -58,13 +58,13 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/createStudent" element={<CreateStudent />} />
-        <Route path="/updateStudent/:id" element={<UpdateStudent />} />
-        <Route path="/readStudents" element={<ListStudent />} />
-        <Route path="/createProfessor" element={<CreateProfessor />} />
-        <Route path="/updateProfessor/:id" element={<UpdateProfessor />} />
-        <Route path="/readProfessors" element={<ListProfessor />} />
+        <Route path="about" element={<About />} />
+        <Route path="createStudent" element={<CreateStudent />} />
+        <Route path="listStudent" element={<ReadStudent />} />
+        <Route path="editStudent/:id" element={<UpdateStudent />} />
+        <Route path="createProfessor" element={<CreateProfessor />} />
+        <Route path="listProfessor" element={<ReadProfessor />} />
+        <Route path="editProfessor/:id" element={<UpdateProfessor />} />
       </Routes>
     </div>
   );
